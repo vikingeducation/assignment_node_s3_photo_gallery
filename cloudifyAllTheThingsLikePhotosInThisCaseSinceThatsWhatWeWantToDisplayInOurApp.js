7e11a3e3fe59bdfc9067ed3d295f0908da9b7109
EdTriplett <EdTriplett@users.noreply.github.com>
2017-09-12 13:55:17 -0400

    fileuploader

--- cloudifyAllTheThingsLikePhotosInThisCaseSinceThatsWhatWeWantToDisplayInOurApp.js
+++ cloudifyAllTheThingsLikePhotosInThisCaseSinceThatsWhatWeWantToDisplayInOurApp.js
@@ -19,8 +19,47 @@ const mw = upload.single("photo[file]");
 
 // do stuff
 
-const FileUploader = {
-  // add functions
+const PHOTO_DATA_PATH = path.resolve('./data/photos.json');
+
+const _writePhotoDataFile = (photos) => {
+  fs.writeFileSync(
+    PHOTO_DATA_PATH,
+    JSON.stringify(photos, null, 2)
+  );
+};
+
+if (!fs.existsSync(PHOTO_DATA_PATH)) {
+  _writePhotoDataFile({});
+}
+
+ // add functions
+const FileUploader = {};
+
+FileUploader.upload = (file)=>{
+  const extension = mime.extension(file.mimetype);
+  const filename = path.parse(file.name).name;
+  return new Promise((resolve, reject)=>{
+    const options = {
+      Bucket: bucket,
+      Key: `${filename}-${md5(Date.now())}.${extension}`,
+      Body: file.date
+    };
+
+    s3.upload(options, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        const photos = require(PHOTO_DATA_PATH);
+        const photo = {
+          url: data.Location,
+          name: data.key
+        };
+        photos[data.key] = photo;
+        _writePhotoDataFile(photos);
+        resolve(photo);
+      }
+    });
+  });
 };
 
 module.exports = FileUploader;
