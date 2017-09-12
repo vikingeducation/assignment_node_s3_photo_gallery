9e8ecbbd12114eeb7e8e6c4d26c4ec1831ab7eb8
Eric Glover <etgt28@mail.missouri.edu>
2017-09-12 17:22:01 -0500

    problems mongo

--- models/User.js
+++ models/User.js
@@ -1,27 +1,30 @@
-const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 
 const UserSchema = new mongoose.Schema(
-	{
-		username: {
-			type: String,
-			required: true,
-			unique: true
-		},
-		password: String,
-		photos: [
-			{
-				key: String,
-				filename: String,
-				description: String,
-				url: String
-			}
-		]
-	},
-	{
-		timestamps: true
-	}
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    password: String,
+    photos: [
+      {
+        key: String,
+        filename: String,
+        description: String,
+        url: String
+      }
+    ]
+  },
+  {
+    timestamps: true
+  }
 );
 
-const User = mongoose.model('User', UserSchema);
+UserSchema.methods.getPhoto = key =>
+  this.photos.find(photo => photo.key === key);
+
+const User = mongoose.model("User", UserSchema);
 module.exports = User;
