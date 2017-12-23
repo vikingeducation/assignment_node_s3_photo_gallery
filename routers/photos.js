fa473de2263a9aac446663e47b1fcc35f5f441fb
maddiereddy <maddiereddy@gmail.com>
2017-12-22 17:45:24 -0800

    file uploads setup done

---
+++ routers/photos.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const router = express.Router();
+const FileUpload = require('./../services/file_upload');
+
+
+
+// ----------------------------------------
+// Index
+// ----------------------------------------
+router.get(['/', '/photos'], (req, res) => {
+  const photos = require('./../data/photos');
+  console.log(photos);
+  res.render('photos/index', { photos });
+});
+
+
+// ----------------------------------------
+// New
+// ----------------------------------------
+router.get('/photos/new', (req, res) => {
+  res.render('photos/new');
+});
+
+
+// ----------------------------------------
+// Create
+// ----------------------------------------
+const mw = FileUpload.single('photo[file]');
+router.post('/photos', mw, (req, res, next) => {
+  console.log('Files', req.file);
+
+  FileUpload.upload({
+    data: req.file.buffer,
+    name: req.file.originalname,
+    mimetype: req.file.mimetype
+  })
+    .then((data) => {
+      console.log(data);
+      req.flash('success', 'Photo created!');
+      res.redirect('/photos');
+    })
+    .catch(next);
+});
+
+
+// ----------------------------------------
+// Destroy
+// ----------------------------------------
+router.delete('/photos/:id', (req, res, next) => {
+  FileUpload.remove(req.params.id)
+    .then(() => {
+      res.redirect('/photos');
+    })
+    .catch(next);
+});
+
+
+
+module.exports = router;
+
+
+
+
+
---
+++ services/file_upload.js
@@ -0,0 +1,132 @@
+const AWS = require('aws-sdk');
+AWS.config.update({ region: "us-west-1" });
+//const s3 = new AWS.S3();
+
+let s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
+
+const bucket = process.env.AWS_S3_BUCKET;
+const mime = require('mime');
+const path = require('path');
+const md5 = require('md5');
+const fs = require('fs');
+const _ = require('lodash');
+
+console.log("Bucket: " + bucket)
+
+const multer = require('multer');
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+
+// Helpers for managing the file-based
+// database for storing photo data
+const PHOTO_DATA_PATH = path.resolve('./data/photos.json');
+
+const _writePhotoDataFile = (photos) => {
+  fs.writeFileSync(
+    PHOTO_DATA_PATH,
+    JSON.stringify(photos, null, 2)
+  );
+};
+
+if (!fs.existsSync(PHOTO_DATA_PATH)) {
+  _writePhotoDataFile({});
+}
+
+
+const FileUploader = {};
+
+
+FileUploader.single = (field) => upload.single(field);
+
+
+FileUploader.upload = (file) => {
+
+  // Use the mime library to get the correct
+  // extension for the mimetype
+  const extension = mime.extension(file.mimetype);
+
+  // Use the path library to get a consistent
+  // file name
+  const filename = path.parse(file.name).name;
+
+  return new Promise((resolve, reject) => {
+
+    // Configure the S3 request options
+    const options = {
+      Bucket: bucket,
+
+      // Use the md5 library to create a unique
+      // hash for this file name and attach
+      // the extension
+      Key: `${ filename }-${ md5(Date.now()) }.${ extension }`,
+      Body: file.data
+    };
+
+    // Upload the file
+    s3.upload(options, (err, data) => {
+
+      // If there's an error
+      // reject the promise
+      if (err) {
+        reject(err);
+      } else {
+
+        // Else we're going to
+        // write the data to a file
+        // (instead of using a database)
+        const photos = require(PHOTO_DATA_PATH);
+        const photo = {
+          url: data.Location,
+          name: data.key
+        };
+        photos[data.key] = photo;
+        _writePhotoDataFile(photos);
+
+        // Resolve the photo data
+        resolve(photo);
+      }
+    });
+  });
+};
+
+
+FileUploader.remove = (id) => {
+
+  // Configure the request
+  const options = {
+    Bucket: bucket,
+    Key: id
+  };
+
+  return new Promise((resolve, reject) => {
+    s3.deleteObject(options, (err, data) => {
+
+      // Reject if error
+      if (err) {
+        reject(err);
+      } else {
+
+        // Delete the photo from the JSON
+        // file-based database
+        // if successful and resolve
+        // the photo data
+        const photos = require(PHOTO_DATA_PATH);
+        const photo = _.clone(photos[id]);
+        delete photos[id];
+        _writePhotoDataFile(photos);
+        resolve(photo);
+      }
+    });
+  });
+};
+
+
+
+module.exports = FileUploader;
+
+
+
